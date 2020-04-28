7804b01b63b683c0fe89908440b59c5615193a7e
Florent Jaby <florent.jaby@gmail.com>
2020-04-28 23:50:28 +0200

    Good initial render

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { IndexComponent } from './page/index/index.component';
+import { ListComponent } from './page/list/list.component';
+import { DetailComponent } from './page/list/detail/detail.component';
+
 
 
 const routes: Routes = [{
   path: '',
+  pathMatch: 'full',
   component: IndexComponent
+}, {
+  path: 'list',
+  component: ListComponent,
+  children: [{
+    path: 'detail',
+    component: DetailComponent
+  }]
 }];
 
 @NgModule({
